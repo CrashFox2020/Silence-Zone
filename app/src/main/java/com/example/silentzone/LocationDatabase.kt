324d5942d15560b3369316b1e38392c86d69d50d
bharat <bharatjude@gmail.com>
2023-12-22 00:32:49 +0530

    Added location database

--- app/src/main/java/com/example/silentzone/LocationDatabase.kt
+++ app/src/main/java/com/example/silentzone/LocationDatabase.kt
@@ -1,4 +1,12 @@
 package com.example.silentzone
 
-class LocationDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [LocationData::class],
+    version = 1
+)
+abstract class LocationDatabase: RoomDatabase() {
+    abstract val dao:LocationDao
 }
